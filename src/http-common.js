0499b53dae01026c0b90f71cac35f3d366c00e1c
Canabisdev <black-batman@localhost.localdomain>
2022-05-16 00:33:58 +0300

    deploy react

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/',
+  baseURL: process.env.REACT_APP_API_URL || 'http://44.204.147.127/api/v1/',
   headers: {
     'Content-type': 'application/json'
   }
